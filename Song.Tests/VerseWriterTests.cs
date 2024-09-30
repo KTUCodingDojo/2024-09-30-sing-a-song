04572612c956dbcbe1da7994ef384774acf9cbc0
Evaldas Guogis <evaldasguogis@yahoo.com>
2024-09-30 19:31:46 +0300

    test: fixed test for second rhyme

--- Song.Tests/VerseWriterTests.cs
+++ Song.Tests/VerseWriterTests.cs
@@ -25,6 +25,26 @@ I don't know why she swallowed a fly - perhaps she'll die!";
 
         }
 
+        [Fact]
+        public void WriteSecondVerse()
+        {
+            string expected = @"There was an old lady who swallowed a spider;
+That wriggled and wiggled and tickled inside her.
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!";
+
+            VerseWriter writer = new VerseWriter();
+
+            List<string> animals = ["fly", "spider"];
+            List<string> animalLines = ["", "That wriggled and wiggled and tickled inside her."];
+
+
+            string actual = writer.WriteVerse(animals, animalLines);
+
+            Assert.Equal(expected, actual);
+
+        }
+
 
     }
 }
--- Song/VerseWriter.cs
+++ Song/VerseWriter.cs
@@ -11,8 +11,23 @@ namespace Song
     {
         public string WriteVerse(List<string> animals, List<string> animalLines)
         {
-            return $"There was an old lady who swallowed a {animals[0]}.{Environment.NewLine}" +
-                 $"I don't know why she swallowed a {animals[0]} - perhaps she'll die!";
+            string separator = animals.Count == 1 ? "." : ";";
+            string secondSeparator;
+            string firstLine = $"There was an old lady who swallowed a {animals[animals.Count - 1]}{separator}{Environment.NewLine}";
+            string secondLine = "";
+            if(animals.Count > 1)
+            {
+                secondLine = $"{animalLines[animalLines.Count - 1]}{Environment.NewLine}";
+            }
+            string rhymeLines = "";
+            for (int i = animalLines.Count - 1; i > 0; i--)
+            {
+                secondSeparator = i == 1 ? ";" : ",";
+                rhymeLines += $"She swallowed the {animals[i]} to catch the {animals[i - 1]}{secondSeparator}{Environment.NewLine}";
+            }
+            string lastLine = $"I don't know why she swallowed a {animals[0]} - perhaps she'll die!";
+
+            return firstLine + secondLine + rhymeLines + lastLine;
         }
 
     }
