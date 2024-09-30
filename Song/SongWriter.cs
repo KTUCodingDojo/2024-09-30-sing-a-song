3f118cd9207dfab228fcb11612ec4f184cee34c8
Evaldas Guogis <evaldasguogis@yahoo.com>
2024-09-30 18:59:06 +0300

    Added VerseWriter

--- Song/SongWriter.cs
+++ Song/SongWriter.cs
@@ -11,8 +11,8 @@ namespace Song
     {
         public string Sing(string animal, string animalLine)
         {
-            return $"There was an old lady who swallowed a {animal}.{Environment.NewLine}" +
-                 $"I don't know why she swallowed a {animal} - perhaps she'll die!";
+            VerseWriter verserWriter = new VerseWriter();
+            return verserWriter.WriteVerse(animal, animalLine);
         }
     }
 }
