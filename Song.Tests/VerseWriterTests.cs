4510fa2ab70d656a0bd79c5a0659846997135ec6
Juozas Balčikonis <juobal3@ktu.lt>
2024-10-01 18:08:12 +0300

    Created a test, testing third verse.

--- Song.Tests/VerseWriterTests.cs
+++ Song.Tests/VerseWriterTests.cs
@@ -26,7 +26,7 @@ I don't know why she swallowed a fly - perhaps she'll die!";
         }
 
         [Fact]
-        public void WriteSecondVerse()
+        public void WriteSecondVerse() // needs more code, prob
         {
             string expected = @"There was an old lady who swallowed a spider;
 That wriggled and wiggled and tickled inside her.
@@ -45,6 +45,26 @@ I don't know why she swallowed a fly - perhaps she'll die!";
 
         }
 
+        [Fact]
+        public void WriteThirdVerse() // needs more code, prob
+        {
+            string expected = @"There was an old lady who swallowed a bird;
+How absurd to swallow a bird.
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!";
+
+            VerseWriter writer = new VerseWriter();
+
+            List<string> animals = ["fly", "spider", "bird"];
+            List<string> animalLines = ["", "That wriggled and wiggled and tickled inside her.", "How absurd to swallow a bird."];
+
+
+            string actual = writer.WriteVerse(animals, animalLines);
+
+            Assert.Equal(expected, actual);
+
+        }
 
     }
 }
--- Song/Program.cs
+++ Song/Program.cs
@@ -4,7 +4,7 @@ namespace Song
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) // juozas testuoja
         {
             var song = @"There was an old lady who swallowed a fly.
 I don't know why she swallowed a fly - perhaps she'll die!
