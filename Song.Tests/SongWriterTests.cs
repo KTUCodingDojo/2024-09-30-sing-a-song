690434e783e0aba15c4dbbb147005b442ede9d7c
Juozas Balčikonis <juobal3@ktu.lt>
2024-10-01 18:35:09 +0300

    Added test testing 4 verses.

--- Song.Tests/SongWriterTests.cs
+++ Song.Tests/SongWriterTests.cs
@@ -53,6 +53,37 @@ She swallowed the spider to catch the fly;
 I don't know why she swallowed a fly - perhaps she'll die!", actual);
 
         }
+        [Fact]
+        public void SongWith4AnimalsTwoVerses()
+        {
+            SongWriter writer = new SongWriter();
+            List<string> animals = ["fly", "spider", "bird", "cat"];
+            List<string> animalLines = ["", "That wriggled and wiggled and tickled inside her.", "How absurd to swallow a bird.", "Fancy that to swallow a cat!"];
+
+            string actual = writer.Sing(animals, animalLines);
+
+            Assert.Equal(@"There was an old lady who swallowed a fly.
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a spider;
+That wriggled and wiggled and tickled inside her.
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a bird;
+How absurd to swallow a bird.
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a cat;
+Fancy that to swallow a cat!
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!", actual);
+
+        }
 
 
     }
