08f71756fc45c60c0fbb311ab72d06b2a0efe732
shush2u <arnassiusa@gmail.com>
2024-10-10 23:57:11 +0300

    Implemented approval test.

--- Song/Program.cs
+++ Song/Program.cs
@@ -6,48 +6,24 @@ namespace Song
     {
         public static void Main(string[] args)
         {
-            var song = @"There was an old lady who swallowed a fly.
-I don't know why she swallowed a fly - perhaps she'll die!
+            SongWriter songWriter = new SongWriter();
 
-There was an old lady who swallowed a spider;
-That wriggled and wiggled and tickled inside her.
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+            var animals = new List<(string, string)>
+            {
+                ("fly", ""),
+                ("spider", "That wriggled and wiggled and tickled inside her."),
+                ("bird", "How absurd to swallow a bird."),
+                ("cat", "Fancy that to swallow a cat!"),
+                ("dog", "What a hog, to swallow a dog!"),
+                ("cow", "I don't know how she swallowed a cow!"),
+            };
 
-There was an old lady who swallowed a bird;
-How absurd to swallow a bird.
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+            foreach (var animal in animals)
+            {
+                songWriter.Sing(animal.Item1, animal.Item2);
+            }
 
-There was an old lady who swallowed a cat;
-Fancy that to swallow a cat!
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
-
-There was an old lady who swallowed a dog;
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
-
-There was an old lady who swallowed a cow;
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
-
-There was an old lady who swallowed a horse...
-...She's dead, of course!";
-
-            Console.WriteLine(song);
+            Console.WriteLine(songWriter.FinishSong("horse"));
         }
     }
 }
