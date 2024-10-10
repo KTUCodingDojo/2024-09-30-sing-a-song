13d3086c78d01092fe210f9738706f7bcfe001bc
shush2u <arnassiusa@gmail.com>
2024-10-11 00:51:00 +0300

    Inverted If in WriteVerse().

--- Song/VerseWriter.cs
+++ Song/VerseWriter.cs
@@ -17,50 +17,24 @@ namespace Song
 
             sb.Append($"There was an old lady who swallowed a {animals[animals.Count - 1].Name}");
 
-            if(animals.Count >= 2)
-            {
-                sb.AppendLine(";");
-
-                sb.AppendLine(animals[animals.Count - 1].UniqueLine);
-
-                for (int i = animals.Count - 1; i > 1; i--)
-                {
-                    sb.AppendLine($"She swallowed the {animals[i]} to catch the {animals[i - 1]},");
-                }
-                sb.AppendLine($"She swallowed the {animals[1]} to catch the {animals[0]};");
-
-            }
-            else
+            if(animals.Count == 1)
             {
                 sb.AppendLine(".");
+                sb.Append($"I don't know why she swallowed a {animals[0]} - perhaps she'll die!");
+                return sb.ToString();
             }
 
+            sb.AppendLine(";");
+
+            sb.AppendLine(animals[animals.Count - 1].UniqueLine);
+
+            for (int i = animals.Count - 1; i > 1; i--)
+            {
+                sb.AppendLine($"She swallowed the {animals[i]} to catch the {animals[i - 1]},");
+            }
+            sb.AppendLine($"She swallowed the {animals[1]} to catch the {animals[0]};");
+
             sb.Append($"I don't know why she swallowed a {animals[0]} - perhaps she'll die!");
-
-
-            return sb.ToString();
-
-            //Animal currentAnimal = animals.Pop();
-
-            //sb.Append($"There was an old lady who swallowed a {currentAnimal}");
-
-            //if(animals.Count == 0)
-            //{
-            //    sb.AppendLine(".");
-            //    sb.Append($"I don't know why she swallowed a {currentAnimal} - perhaps she'll die!");
-            //    return sb.ToString();
-            //}
-
-            //sb.AppendLine(";");
-            //sb.AppendLine(currentAnimal.UniqueLine);
-
-            //while (animals.Count > 1) 
-            //{
-            //    sb.AppendLine($"She swallowed the {currentAnimal} to catch the {currentAnimal = animals.Pop()},");
-            //}
-            //sb.AppendLine($"She swallowed the {currentAnimal} to catch the {currentAnimal = animals.Pop()};");
-
-            //sb.Append($"I don't know why she swallowed a {currentAnimal} - perhaps she'll die!");
 
             return sb.ToString();
         }
